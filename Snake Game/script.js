8c6a80571ecd915c7ae01c6b19fda384b92befdf
Abdulkadir Aydemir <111640113+AbdulkadirAydemir@users.noreply.github.com>
2023-06-22 16:21:05 +0300

    Update script.js

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -26,3 +26,40 @@ const handleGameOver = () => {
     alert("Game Over! Yeniden oynatmak için Tamam'a basın...");
     location.reload();
 }
+
+const chanceDirection = e => {
+    //? Tuşa basmaya bağlı olarak hız değerini değiştirme
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        veloCityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        veloCityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && veloCityX != 1) {
+        veloCityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight" && veloCityX != -1) {
+        veloCityX = 1;
+        velocityY = 0;
+    }
+}
+
+//? // Her tuş tıklamasında changeDirection'ı çağırmak ve anahtar veri kümesi değerini bir nesne olarak iletmek
+controls.forEach(button => button.addEventListener("click",
+    () => chanceDirection({ key: button.dataset.key })));
+
+const initGame = () => {
+    if (gameOver) return handleGameOver();
+    let html = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}" ></div>`;
+
+    //! Yılanın yiyeceğe çarpıp çarpmadığını kontrol etme
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]);
+        score++;
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score : ${score}`;
+        highScoreElement.innerText = `High Score : ${highScore}`;
+    }
+}
